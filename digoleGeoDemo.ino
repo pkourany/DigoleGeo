3d0e5ce71af89c0aa5a61471ba54a747972d6e49
Paul Kourany <pkourany@users.noreply.github.com>
2016-02-04 20:54:24 -0500

    Support for new displays

--- digoleGeoDemo.ino
+++ digoleGeoDemo.ino
@@ -1,22 +1,9 @@
-/* *********************************************************************************** */
-/* Digole Serial Display Library - Version 006                                         */
-/* Copyright 2014 Paul Kourany / Timothy Brown / Digole                                */
-/* *********************************************************************************** */
-/* Setup the class for your display *before* void setup():                             */
-/*                                                                                     */
-/* DigoleSerialDisp digole(arguments);                                                 */
-/*                                                                                     */
-/* Arguments:                                                                          */
-/*                                                                                     */
-/* [SPI] Chip Select Pin (SS for the default pin, 255 if CS is hardwired low.)         */
-/* [SoftSPI] Data Pin, Clock Pin, Chip Select Pin (255 if CS is hardwired low.)        */
-/* [I2C] Address of the Display (Default: 0x27)                                        */
-/* [UART] Baud Rate [9600 to 115200]                                                   */
-/*                                                                                     */
-/* To use, call digole.begin(); *inside* void setup(); to start the display. */
-/* You may also call digole.end(); to release the pins and clear the I2C/SPI/UART bus. */
-/*                                                                                     */
-/* *********************************************************************************** */
+/*********************************************************************/
+/* Digole Serial Display Library with Graphic Primitives Extensions
+/* Demo program
+/* by Paul Kourany (peekay123), Feb 4, 2016
+/* Digole Digital Solutions: www.digole.com
+/*********************************************************************/
 
 /* Uncomment the #define below for the desired interface: SPI, SoftSPI, I2C, UART. */
 
@@ -24,20 +11,7 @@
 //#define _Digole_Serial_SoftSPI_
 //#define _Digole_Serial_I2C_
 #define _Digole_Serial_UART_
-
-/* Be sure to #include "DigoleSerialDisp.h" *after* the #define above or your compile will fail. */
-/* !!! Don't forget to set display width and display height in file DigoleSerialDisp.h !!!!! */
-
 #include "DigoleGeo.h"
-
-/* Below are sane defaults for the various interfaces. Uncomment one and customize as needed! */
-//DigoleSerialDisp mydisp(D2, D3, SS);			//SOFT SPI setup - specify DATA, CLOCK, SS pins
-//DigoleSerialDisp mydisp(SS);					//Hardware SPI setup - specify SS pin
-//DigoleSerialDisp mydisp('\x27');				//I2C specified - specify Pointer to Wire object, I2C address of Digole
-DigoleSerialDisp mydisp(&Serial1, 115200);	//USART (Serial 1 or 2) - specify &Serial1 or &Serial2 and baudrate
-
-#define _TEXT_ 0
-#define _GRAPH_ 1
 
 //Colours defined as 8-bit value RRRG GGBB
 #define VGA_BLACK   0x00
@@ -50,16 +24,24 @@ DigoleSerialDisp mydisp(&Serial1, 115200);	//USART (Serial 1 or 2) - specify &Se
 #define VGA_LIME    0x14
 #define VGA_YELLOW  0xFC
 
-const int max_x = DISP_W;
-const int max_y = DISP_H;
 
+int max_x, max_y;
 
+//DigoleSerialDisp mydisp(D2, D3, SS);      //SOFT SPI setup - specify DATA, CLOCK, SS pins
+//DigoleSerialDisp mydisp(SS);              //Hardware SPI setup - specify SS pin
+//DigoleSerialDisp mydisp('\x27');    //I2C specified - specify Pointer to Wire object, I2C address of Digole
+DigoleSerialDisp mydisp(&Serial1, 115200);    //USART (Serial 1 or 2) - specify &Serial1 or &Serial2 and baudrate
+
+ 
 void setup() {
+  Serial.begin(9600);
   mydisp.begin();
+  max_x = mydisp.getNativeDisplayWidth();
+  max_y = mydisp.getNativeDisplayHeight();
   mydisp.backLightOn();
   mydisp.setColor(1);
 }
-
+ 
 void loop() {
 
   int x1,x2,x3,y1,y2,y3,r,as,ae;
@@ -67,16 +49,16 @@ void loop() {
   mydisp.clearScreen(); //CLear screen
   mydisp.disableCursor();
 
-// Draw some random triangles
+// Draw some random round rectangles
   for (int i=0; i<50; i++)
-  {
+  { 
     mydisp.setTrueColor(random(255), random(255), random(255));
     x1=random(max_x);
     y1=random(max_y);
     x2=random(max_x);
     y2=random(max_y);
     mydisp.drawRoundRect(x1, y1, x2, y2);
-    delay(10);
+    //delay(15);
   }
 
   delay(1000);
@@ -91,7 +73,7 @@ void loop() {
     x2=random(max_x);
     y2=random(max_y);
     mydisp.fillRoundRect(x1, y1, x2, y2);
-    delay(15);
+    //delay(20);
   }
 
   delay(1000);
@@ -124,7 +106,7 @@ void loop() {
     x3=random(max_x);
     y3=random(max_y);
     mydisp.fillTriangle(x1, y1, x2, y2, x3, y3);
-    delay(15);
+    //delay(25);
   }
 
   delay(1000);
@@ -138,8 +120,9 @@ void loop() {
     y1=30+random(max_y-60);
     r=10+random(20);
     as=random(360);
-    ae=random(360);    mydisp.drawPie(x1, y1, r, as, ae);
-    delay(10);
+    ae=random(360);
+    mydisp.drawPie(x1, y1, r, as, ae);
+   //delay(10);
   }
 
   delay(1000);
@@ -186,7 +169,7 @@ void loop() {
 
   delay(1000);
   mydisp.clearScreen(); //CLear screen
-
+  
 // Draw some random ellipses
   for (int i=0; i<50; i++)
   {
@@ -196,12 +179,12 @@ void loop() {
     x2=5+random(30);
     y2=5+random(30);
     mydisp.drawEllipse(x1, y1, x2, y2);
-    delay(100);
+    //delay(100);
   }
 
-  delay(2000);
+  delay(1000);
   mydisp.clearScreen(); //CLear screen
-
+  
 // Draw some random filled ellipses
   for (int i=0; i<50; i++)
   {
@@ -211,7 +194,7 @@ void loop() {
     x2=5+random(30);
     y2=5+random(30);
     mydisp.drawFilledEllipse(x1, y1, x2, y2);
-    delay(100);
+    //delay(100);
   }
-
+  delay(1000);
 }
